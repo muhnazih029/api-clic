c4c946117754974d5e695b0e15dcbb6520fae8f2
ballll16 <ahmadhamidbalya@gmail.com>
2025-01-06 17:11:38 +0700

    test: 🧪 example

--- test/example.e2e.spec.ts
+++ test/example.e2e.spec.ts
@@ -1,7 +1,21 @@
+import { logger } from 'src/providers/logger.provider';
 import { describe, expect, it } from 'bun:test';
+import app from 'src';
 
 describe('example', () => {
   it('should pass', () => {
+    logger.info('test');
     expect(1 + 1).toBe(2);
+  });
+  describe('[Example API] GET /', () => {
+    it('should get output', async () => {
+      const res = await app.request('/', {
+        method: 'GET',
+      });
+      logger.info('Example API', res);
+
+      expect(res.status).toBe(200);
+      expect(await res.text()).toBe('Hello Hono!');
+    });
   });
 });
